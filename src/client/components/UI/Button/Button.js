b5664f984ea481dbd5b733a481de611658fbb3b1
Asmazabin Qureshi <aqureshi@1800flowers.com>
2020-08-13 10:28:10 +0530

    button updated

--- src/client/components/UI/Button/Button.js
+++ src/client/components/UI/Button/Button.js
@@ -2,18 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Button.scss';
 
-const Button = (props) => (
-	<Button 
+const button = (props) => (
+	<button 
 		disabled={props.disabled}
 		className={['Button', props.btnType].join(' ')}
-		onClick={props.clicked}>{props.children}</Button>
+		onClick={props.clicked}>{props.children}</button>
 );
 
-Button.propTypes = {
+button.propTypes = {
 	clicked: PropTypes.func,
 	btnType: PropTypes.string,
 	disabled: PropTypes.bool,
 	children: PropTypes.string
 }
 
-export default Button;
+export default button;
